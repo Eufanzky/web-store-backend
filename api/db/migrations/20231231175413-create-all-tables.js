8ad66f924b6d4d665ff70400b8fa3396640c8aae
eufanzky <eufanzky@gmail.com>
2024-01-02 12:02:05 -0400

    fixed migrations

--- api/db/migrations/20231231175413-create-all-tables.js
+++ api/db/migrations/20231231175413-create-all-tables.js
@@ -1,36 +1,62 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+const { Model, DataTypes, Sequelize } = require('sequelize');
+  
 
 const { UserSchema, USER_TABLE } = require('../models/user.model');
 const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');
 const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
-const { MyOrderSchema, MY_ORDER_TABLE } = require('../models/my-order.model');
-const { CustomerSchema, CUSTOMER_TABLE } = require('./../models/customer.model');
+const {
+  CustomerSchema,
+  CUSTOMER_TABLE,
+} = require('./../models/customer.model');
 const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
-const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
-
-
+const {
+  OrderProductSchema,
+  ORDER_PRODUCT_TABLE,
+} = require('../models/order-product.model');
 
 module.exports = {
   async up(queryInterface) {
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(USER_TABLE, UserSchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
-    await queryInterface.createTable(MY_ORDER_TABLE, MyOrderSchema);
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
-    
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable(USER_TABLE);
-    await queryInterface.dropTable(PRODUCT_TABLE);
-    await queryInterface.dropTable(CATEGORY_TABLE);
-    await queryInterface.dropTable(MY_ORDER_TABLE);
-    await queryInterface.dropTable(CUSTOMER_TABLE);
-    await queryInterface.dropTable(ORDER_TABLE);
     await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
+    await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(ORDER_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
+    await queryInterface.dropTable(USER_TABLE);
   },
 };
